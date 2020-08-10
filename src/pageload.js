2a073ecf8f5508a73c8859ffd8d864f93b07e4de
zderApfel <scoutt172@yahoo.com>
2020-08-10 16:29:57 -0400

    Modified CSS a bit

--- src/pageload.js
+++ src/pageload.js
@@ -6,19 +6,6 @@ const loadHomePage = () => {
     borgers.setAttribute('alt', 'Juicy borgers!');
     borgers.id = 'borger-pic';
 
-    const menuLink = document.createElement('h3');
-    menuLink.id = 'menu-link';
-    menuLink.innerHTML = 'Menu';
-    
-    const contactLink = document.createElement('h3');
-    contactLink.id = 'contact-link';
-    contactLink.innerHTML = 'Contact';
-
-    const linkContainer = document.createElement('div');
-    linkContainer.id = 'link-container';
-    linkContainer.appendChild(menuLink);
-    linkContainer.appendChild(contactLink);
-
     const mainTitle = document.createElement('h1');
     mainTitle.innerHTML = "Zach's Borgers";
 
@@ -31,7 +18,7 @@ const loadHomePage = () => {
     const whatABorgerIs = document.createElement('p');
     whatABorgerIs.innerHTML = "Zach gets this question a lot, it's asked a lot by many customers, in fact. The fact of the matter is that it's not important what a borger is. They taste good, so why ask? Nobody has ever gotten sick from a borger, and nobody is allergic to them either. It's impossible to say any of those things has happened. So please, just calm down and eat your borger."
 
-    appendChildren(container, [borgers, linkContainer, mainTitle, intro, whatIsBorger, whatABorgerIs]);
+    appendChildren(container, [borgers, mainTitle, intro, whatIsBorger, whatABorgerIs]);
 }
 
 function appendChildren(parent, array){
