5a26d43686577594228c0471c843788bfb445e87
zderApfel <scoutt172@yahoo.com>
2020-08-11 17:42:04 -0400

    I don't remember what I did here tbh

--- src/pageload.js
+++ src/pageload.js
@@ -1,36 +1,58 @@
 const textStorage = require('./text.json');
 
-const loadBanner = () => {
+function appendChildren(parent, array){ //Utility function doing a mass "append children" so I don't have to type it multiple times
+    for (let x in array){
+        parent.appendChild(array[x]);
+    }
+}
+
+const loadBanner = (defaultActive) => {
+    /*
+        Page IDs:
+            * Home Page: 't2';
+            * Menu Page: 't1';
+            * Contact Page: 't3';
+    */
+    
     const PARENT_BANNER = document.getElementById('tab-container');
     let childrenArray = [];
 
     const menuTab = document.createElement('td');
     menuTab.setAttribute('class', 'not-selected');
     menuTab.textContent = "Menu";
+    menuTab.id = "t1";
     menuTab.addEventListener("click", function(){pageChange(menuTab)});
 
     const homeTab = document.createElement('td');
     homeTab.setAttribute('class', 'not-selected');
     homeTab.textContent = "Home";
+    homeTab.id = "t2";
     homeTab.addEventListener("click", function(){pageChange(homeTab)});
 
     const contactTab = document.createElement('td');
     contactTab.setAttribute('class', 'not-selected');
     contactTab.textContent = "Contact Us!";
-    contactTab.addEventListener("click", function(){pageChange(contactTab)})
+    contactTab.id = "t3";
+    contactTab.addEventListener("click", function(){selectTab(contactTab)});
 
     childrenArray.push(menuTab, homeTab, contactTab);
+
+    for (let x = 0; x < childrenArray.length; x++){
+        if (childrenArray[x].id == defaultActive){
+            childrenArray[x].setAttribute('class', 'selected');
+        }
+    }
+
     appendChildren(PARENT_BANNER, childrenArray);
 
-    function pageChange(tab){
-        tab.setAttribute('class', 'selected');
+    function selectTab(tab){
         console.log("Changing to " + tab.textContent);
     }
 
 }
 
 const loadHomePage = () => {
-    loadBanner();
+    loadBanner('t2');
     const container = document.getElementById('content');
     let childrenArray = []
 
@@ -53,12 +75,6 @@ const loadHomePage = () => {
 
     childrenArray.push(borgers, mainTitle, intro, whatIsBorger, whatABorgerIs);
     appendChildren(container, childrenArray);
-}
-
-function appendChildren(parent, array){
-    for (let x in array){
-        parent.appendChild(array[x]);
-    }
 }
 
 export { loadHomePage };
